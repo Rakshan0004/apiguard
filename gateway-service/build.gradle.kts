58b19a59f36c2db59550f0e7f4e9e962bdd714c3
headshigh <nischalgautam7200@gmail.com>
2026-05-06 16:42:20 +0545

    fixes in gateway and usage service

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -1,10 +1,21 @@
 plugins {
     id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+val springCloudVersion = "2024.0.0"
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.1.5")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation(project(":common"))
 }
+
+
